cbadb0d6c87c76e8a4b39a79b041cfe59857af3b
Максим Иванов <omaxphp@yandex.ru>
2026-09-13 14:30:15 +0300

    feat: bootstrap dadata demo

---
+++ projects/demo/src/main.ts
@@ -0,0 +1,17 @@
+import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {provideTaiga} from '@taiga-ui/core';
+import {provideTuiDaData} from '@taiga-ui/dadata';
+
+import {AppComponent} from './app/app.component';
+import {DADATA_TOKEN} from './app/dadata-token';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideAnimations(),
+        provideHttpClient(),
+        provideTaiga(),
+        provideTuiDaData({token: () => DADATA_TOKEN()}),
+    ],
+}).catch(console.error);
