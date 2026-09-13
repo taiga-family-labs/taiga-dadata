36a8791e589a395ad59b147d0c74a0214081f908
Максим Иванов <omaxphp@yandex.ru>
2026-09-13 15:18:15 +0300

    fix: use taiga-ui-labs provider import

--- projects/demo/src/main.ts
+++ projects/demo/src/main.ts
@@ -2,7 +2,7 @@ import {provideHttpClient} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideTaiga} from '@taiga-ui/core';
-import {provideTuiDaData} from '@taiga-ui/dadata';
+import {provideTuiDaData} from '@taiga-ui-labs/dadata';
 
 import {AppComponent} from './app/app.component';
 import {DADATA_TOKEN} from './app/dadata-token';
